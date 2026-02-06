151ae673959f37320feaf6d4c4ec24a5beb4e9f0
freddy <Frederik.Folkers@web.de>
2026-02-06 19:28:47 +0100

    add tree to consoleEnv

--- modules/consoleEnv.nix
+++ modules/consoleEnv.nix
@@ -31,6 +31,7 @@
       jq # to format json
       fzf # search for files in a fuzzy way
       dua # analyse disk file size
+      tree # to list deeper levels
       alsa-utils # for sound stuff (alsamixer)
       dragon-drop
       libqalculate # gives qalc to calculate stuff
