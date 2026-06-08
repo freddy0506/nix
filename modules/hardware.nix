6294a3de933ecc65aea88bcbca836ffacf67387b
freddy <Frederik.Folkers@web.de>
2026-06-08 18:21:05 +0200

    update audio support
    Here I tried to get audio working just to figureout that my laptop broke.
    
    I am keeping this because I think atleast now it works better with hdmi

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -147,8 +147,17 @@
 
 
     # Enable sound with pipewire.
-    # services.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # Uncomment the following line if you want to use JACK applications
+      # jack.enable = true;
+    };
 
     # Enable touchpad support (enabled default in most desktopManager).
     services.libinput.enable = true;
